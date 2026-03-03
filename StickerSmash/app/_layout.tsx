631a381f4d413220de8c9bd5d3cefec722264475
Robert Masters <robertmasters2478@gmail.com>
2026-03-02 23:20:36 -0500

    add routing to an About page

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+      name="index"
+      options={{
+        headerTitle: "Sticker Smash",
+      }}
+      />
+    <Stack.Screen 
+      name="about"
+      options={{
+        headerTitle: "About",
+      }}
+    />
+    </Stack>
+  )
 }
--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello StickerSmash!</Text>
+      <Link href={"/about"} style={styles.button}>
+        Go to About Screen
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  }
+})
