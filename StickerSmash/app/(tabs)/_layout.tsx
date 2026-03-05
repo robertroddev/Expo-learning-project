4bfbbc011406c556ee57e0a258dd0e9b19a74b4e
Robert Masters <robertmasters2478@gmail.com>
2026-03-05 13:22:46 -0500

    change background color of tab bar and header

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -4,8 +4,16 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs 
-     screenOptions={{
+      screenOptions={{
         tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+        },
       }}
     >
       <Tabs.Screen 
