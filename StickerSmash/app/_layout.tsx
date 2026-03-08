ce7887336da5976694915561ed6bf07b4ca9bb83
Robert Masters <robertmasters2478@gmail.com>
2026-03-08 14:38:19 -0400

    removed return to home screen button

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,9 +1,17 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen 
+        name="(tabs)" 
+        options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{}} />
+      </Stack>
+    </>
+
   );
 }
