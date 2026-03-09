defbe01575aaf412eeab7eb795f3fd265db86903
Robert Masters <robertmasters2478@gmail.com>
2026-03-08 23:43:01 -0400

    add image to home page

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,8 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image } from 'expo-image';
+import { StyleSheet, Text, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image}/>
+      </View>
       <Text style={styles.text}>Hello StickerSmash!</Text>
     </View>
   );
@@ -22,5 +28,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: "underline",
     color: "#fff",
-  }
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 })
